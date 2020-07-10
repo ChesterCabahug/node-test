f51b7348e247f4c3478b8ba65ada4c6488418cc8
Chester <chesterdeveloper2000@gmail.com>
2020-07-10 13:49:19 +0800

    learned in practical ways on how the require cache works in node

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// console.log(arguments)
-// console.log(exports)
-// console.log(require)
-// console.log(module)
-// console.log(__filename)
-console.log(__dirname)
+const counterObj = require("./myscript.js")
+
+console.log(counterObj.getCounter()) // 0
+counterObj.incrementCounter() // 0 + 1
+console.log(counterObj.getCounter()) //1
+
+
+const newCounterObj = require("./myscript.js")
+console.log(newCounterObj.getCounter()) // because of the "require cache", instead of getting 0, we'll get 1"
